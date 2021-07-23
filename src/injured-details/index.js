bba27f250acdfec27e07d69f25263aaf3d430ab6
Greg <greg@Gregs-MBP.toastyduck.com>
2021-07-23 14:36:52 -0500

    First combined commit

--- src/injured-details/index.js
+++ src/injured-details/index.js
@@ -7,6 +7,7 @@ import '@servicenow/now-select';
 import '@servicenow/now-checkbox';
 import '@servicenow/now-textarea';
 import '@servicenow/now-radio-buttons';
+import '@servicenow/now-accordion';
 import styles from './styles.scss';
 
 
@@ -23,21 +24,91 @@ const view = (state,{
 				<span>Please fill the boxes</span>
 			</div>
 			<form>
+			<now-accordion heading-level="3" trigger-icon={{"type":"plus-minus","position":"start"}}>
+				<now-accordion-item 
+					header={{"label":"Head","weight":"bold","size":"sm","variant":"secondary"}} 
+					caption={{"label":"Sub-label 1","style":"italic","variant":"primary"}} expanded={false}>	
+					<div id="attack" slot="content">
+						<now-input label="Type of Force" required />
+					</div>
+					<div id="Description" slot="content">
+						<now-textarea label="Short Description" />
+					</div>
+					<div id="image-file" slot="content">
+						<input id="attach_image" type="file" id="image"></input>
+					</div>	
+					<div id="checkBox" slot="content">
+						<now-checkbox label="Officers involved?"></now-checkbox>
+					</div>		
+				</now-accordion-item>
+				<now-accordion-item 
+					header={{"label":"Neck","weight":"bold","size":"sm","variant":"secondary"}} 
+					caption={{"label":"Sub-label 1","style":"italic","variant":"primary"}} expanded={false}>	
+					<div id="attack" slot="content">
+						<now-input label="Type of Force" required />
+					</div>
+					<div id="Description" slot="content">
+						<now-textarea label="Short Description" />
+					</div>
+					<div id="image-file" slot="content">
+						<input id="attach_image" type="file" id="image"></input>
+					</div>	
+					<div id="checkBox" slot="content">
+						<now-checkbox label="Officers involved?"></now-checkbox>
+					</div>		
+				</now-accordion-item>
+				<now-accordion-item 
+					header={{"label":"Torso","weight":"bold","size":"sm","variant":"secondary"}} 
+					caption={{"label":"Sub-label 1","style":"italic","variant":"primary"}}>
+					<div id="attack" slot="content">
+						<now-input label="Type of Force" required />
+					</div>
+					<div id="Description" slot="content">
+						<now-textarea label="Short Description" />
+					</div>
+					<div id="image-file" slot="content">
+						<input id="attach_image" type="file" id="image"></input>
+					</div>
+					<div id="checkBox" slot="content">
+						<now-checkbox label="Officers involved?"></now-checkbox>
+					</div>
+				</now-accordion-item>
+				<now-accordion-item 
+					header={{"label":"Arms","weight":"bold","size":"sm","variant":"secondary"}} 
+					caption={{"label":"Sub-label 1","style":"italic","variant":"primary"}} expanded={false}>	
+					<div id="attack" slot="content">
+						<now-input label="Type of Force" required />
+					</div>
+					<div id="Description" slot="content">
+						<now-textarea label="Short Description" />
+					</div>
+					<div id="image-file" slot="content">
+						<input id="attach_image" type="file" id="image"></input>
+					</div>	
+					<div id="checkBox" slot="content">
+						<now-checkbox label="Officers involved?"></now-checkbox>
+					</div>		
+				</now-accordion-item>
+				<now-accordion-item 
+					header={{"label":"Legs","weight":"bold","size":"sm","variant":"secondary"}} 
+					caption={{"label":"Sub-label 1","style":"italic","variant":"primary"}} expanded={false}>	
+					<div id="attack" slot="content">
+						<now-input label="Type of Force" required />
+					</div>
+					<div id="Description" slot="content">
+						<now-textarea label="Short Description" />
+					</div>
+					<div id="image-file" slot="content">
+						<input id="attach_image" type="file" id="image"></input>
+					</div>	
+					<div id="checkBox" slot="content">
+						<now-checkbox label="Officers involved?"></now-checkbox>
+					</div>		
+				</now-accordion-item>
+				</now-accordion>
 				<div id="Injuries">
 					<now-input id="injure" value={state.properties.injuries} placeholder="Click parts on the image" label="Body parts injured" disabled={true} required />
 					<now-input label="Extent of Injuries" required />
-				</div>
-				<div id="attack">
-					<now-input label="Type of Force" required />
-				</div>
-				<div id="Description">
-					<now-textarea label="Short Description" />
-				</div>
-				<div id="checkBox">
-					<now-checkbox label="Officers involved?"></now-checkbox>
-				</div>
-				<div id="image-file">
-					<input id="attach_image" type="file" id="image"></input>
 				</div>
 				<div>
 					<now-button id="submit" label="Submit" variant="primary" size="md" icon="" tooltip-content=""/>
